0fdc7bf60fb9436b52867f764f59b664f589766d
jacquelynneheiman <77410423+jacquelynneheiman@users.noreply.github.com>
2022-08-17 15:13:12 -0700

    Removed deleted files

--- JacquelynneHeiman_Aeries/JacquelynneHeiman_Aeries/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JacquelynneHeiman_Aeries.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
